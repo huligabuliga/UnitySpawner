a7b0d0fb0c70bcf409d7b33c4b0fd296e9fc30c1
huligabuliga <jonastclausen@gmail.com>
2021-05-05 17:40:59 -0500

    player

---
+++ UnitySpawner/Grupo1_JonasTobiasClausen-A00825449/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    float movement;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        transform.Translate(movement * Time.deltaTime * speed , 0, 0);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9,9), transform.position.y, transform.position.z);
+    }
+}
